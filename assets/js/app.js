647418f3f0683dd95405231eb121913539818080
Wayne Halloran <whalloranirl@protonmail.com>
2023-12-07 11:00:18 +0000

    Added fucntion to check username

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,27 @@ const userNameInput = document.querySelector("#user-name-input");
 const userInputBtn = document.querySelector("#user-input-btn");
 // selecting the emojis picture element
 const emojisPicture = document.querySelector("#emojis");
+
+// function for checking userNameInput has been correctly entered
+const greeting = () => {
+  if (userNameInput.value == "") {
+    let pFirstChild = chatDiv.firstElementChild;
+    pFirstChild.textContent = `Oh I didn't get your name!`;
+  } else {
+    // set the text in the chatDiv to equal the value of the name provided
+    chatDiv.textContent = `Hi ${userNameInput.value}, nice to meet you !`;
+
+    // remove both the input & buttons from the DOM
+    userNameInput.remove();
+    userInputBtn.remove();
+
+    // removing .hide class from gameDiv to display it
+    gameDiv.classList.remove("hide");
+
+    //clear instructionsDiv text
+    instructionsDiv.textContent = "";
+  }
+};
+
+// Event listeners
+userInputBtn.addEventListener("click", greeting);
