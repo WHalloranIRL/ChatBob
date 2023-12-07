839a4f41519e5c6a10e1879e9f3113dd84cbcf0a
Wayne Halloran <whalloranirl@protonmail.com>
2023-12-07 10:46:34 +0000

    Added selectors and variales to app.js

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+// selecting all the section elements in html
+const emojiSection = document.querySelector("#emoji-section");
+const chatSection = document.querySelector("#chat-section");
+const instructionSection = document.querySelector("#instruction-section");
+const gameSection = document.querySelector("#game-section");
+const controlsSection = document.querySelector("#controls-section");
+//selecting all the div elements in html
+const emojiDiv = document.querySelector("#emoji");
+const chatDiv = document.querySelector("#chat");
+const instructionsDiv = document.querySelector("#instructions");
+const gameDiv = document.querySelector("#game");
+const controlsDiv = document.querySelector("#controls");
+//selecting the user input field
+const userNameInput = document.querySelector("#user-name-input");
+const userInputBtn = document.querySelector("#user-input-btn");
+// selecting the emojis picture element
+const emojisPicture = document.querySelector("#emojis");
