ac7f45f0dda354fcfdae6190140acc1a08d6a818
Wayne Halloran <whalloranirl@protonmail.com>
2023-12-12 16:51:08 +0000

    Added quiz.js

---
+++ assets/js/quiz.js
@@ -0,0 +1,109 @@
+const popCultureQuestions = [
+    {
+      questionText: "In which movie does Leonardo DiCaprio say, 'I'm the king of the world!'?",
+      answers: {
+        option1: "Inception",
+        option2: "Titanic",  // Correct answer
+        option3: "The Wolf of Wall Street",
+        option4: "Shutter Island",
+      },
+      correctAnswer: "option2",
+    },
+    {
+      questionText: "Who played the character of Tony Stark in the Marvel Cinematic Universe?",
+      answers: {
+        option1: "Chris Hemsworth",
+        option2: "Chris Evans",
+        option3: "Robert Downey Jr.",  // Correct answer
+        option4: "Mark Ruffalo",
+      },
+      correctAnswer: "option3",
+    },
+    {
+      questionText: "Which TV series features dragons, direwolves, and the Iron Throne?",
+      answers: {
+        option1: "Breaking Bad",
+        option2: "The Walking Dead",
+        option3: "Game of Thrones",  // Correct answer
+        option4: "Stranger Things",
+      },
+      correctAnswer: "option3",
+    },
+    {
+      questionText: "Who is known for saying, 'I feel the need... the need for speed'?",
+      answers: {
+        option1: "Tom Cruise",
+        option2: "Brad Pitt",
+        option3: "Will Smith",
+        option4: "Val Kilmer",  // Correct answer
+      },
+      correctAnswer: "option4",
+    },
+    {
+      questionText: "Which pop star is known as the 'Queen of Pop'?",
+      answers: {
+        option1: "Lady Gaga",
+        option2: "Beyoncé",
+        option3: "Madonna",  // Correct answer
+        option4: "Taylor Swift",
+      },
+      correctAnswer: "option3",
+    },
+    {
+      questionText: "What is the name of the fictional wizarding school in the Harry Potter series?",
+      answers: {
+        option1: "Hogwarts",  // Correct answer
+        option2: "Durmstrang",
+        option3: "Beauxbatons",
+        option4: "Ilvermorny",
+      },
+      correctAnswer: "option1",
+    },
+    {
+      questionText: "Which animated film features a character named Simba?",
+      answers: {
+        option1: "Finding Nemo",
+        option2: "The Lion King",  // Correct answer
+        option3: "Toy Story",
+        option4: "Shrek",
+      },
+      correctAnswer: "option2",
+    },
+    {
+      questionText: "Who is the author of the 'Harry Potter' book series?",
+      answers: {
+        option1: "J.K. Rowling",  // Correct answer
+        option2: "George R.R. Martin",
+        option3: "Stephen King",
+        option4: "Suzanne Collins",
+      },
+      correctAnswer: "option1",
+    },
+    {
+      questionText: "Which superhero is known for his web-slinging abilities?",
+      answers: {
+        option1: "Batman",
+        option2: "Iron Man",
+        option3: "Spider-Man",  // Correct answer
+        option4: "Superman",
+      },
+      correctAnswer: "option3",
+    },
+    {
+      questionText: "What is the name of the fictional wizard detective in a series of novels by J.K. Rowling?",
+      answers: {
+        option1: "Sherlock Holmes",
+        option2: "Hercule Poirot",
+        option3: "Cormoran Strike",
+        option4: "Harry Potter",  // Correct answer
+      },
+      correctAnswer: "option4",
+    },
+  ];
+  
+  // Example usage:
+  console.log(popCultureQuestions[0].questionText);
+  console.log(popCultureQuestions[1].answers.option3);
+  console.log(popCultureQuestions[2].correctAnswer);
+  // Continue as needed for the other questions...
+  
